57ac3556578848fe95dbcef38dd696779a9892a8
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2022-02-24 22:39:36 +0100

    Copy README to doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! A [hyperloglog](https://en.wikipedia.org/wiki/HyperLogLog) implementation in Rust.
+//!
+//! ```
+//! use hyperloglog::HyperLogLog;
+//!
+//! let mut hll = HyperLogLog::<i32>::new();
+//! for i in 0..100_000 {
+//!     hll.insert(&i);
+//! }
+//! assert!((50_000..150_000).contains(&hll.len()));
+//! ```
 pub use crate::{
     error::{TryFromIntError, TryMergeError, TryMergeErrorKind},
     hyperloglog::HyperLogLog,
