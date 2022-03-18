be5c7ce887a723fb43d39e1dda5100bbbba1e607
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2022-03-18 18:52:11 +0100

    More benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -28,3 +28,27 @@ fn count(b: &mut Bencher) {
     }
     b.iter(|| hll.len());
 }
+
+macro_rules! bench_merge {
+    ($meth:ident) => {
+        bench_merge!($meth, $meth);
+    };
+    ($name:ident, $meth:ident) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            let mut hll1 = HyperLogLog::<i32>::new();
+            for i in 0..75_000 {
+                hll1.insert(&i);
+            }
+            let mut hll2 = HyperLogLog::<i32>::new();
+            for i in 25_000..100_000 {
+                hll2.insert(&i);
+            }
+            b.iter(|| hll1.$meth(&hll2))
+        }
+    };
+}
+
+bench_merge!(try_merge);
+
+bench_merge!(try_merge_from);
